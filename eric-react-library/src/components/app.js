a7101ce42c084e90adaf32aa3cef4c6001698d32
Eric Lars Lee <ericlarslee@gmail.com>
2021-06-20 19:38:22 -0500

    refactored more code. Still working on getting page to render currently with hooks

--- eric-react-library/src/components/app.js
+++ eric-react-library/src/components/app.js
@@ -1,102 +1,80 @@
 import React, { useState, useEffect } from 'react';
-// import TitleBar from './TitleBar/titleBar';
 import './app.css';
 import axios from 'axios';
 import Song from './Song/song.js';
 import SongTable from './Table/songTable.js';
-import SongCreator from './SongCreator/songCreator.js';
-import SearchBar from './SearchBar/searchBar';
-import { useState } from 'react';
+import SongCreator from './SongCreator/songCreator';
+// import SongForm from './SongForm';
 
 let styling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
 
 const App = () => {
     const [songs, setSongs] = useState([]);
     const [filterTerm, setFilterTerm] = useState("");
+    // const [songForm, setSongForm] = SongForm({title: '', album: '', artist: '', genre: '', release_date: ''});
     
     
-    const getAllSongs= async() => {
+    async function getAllSongs(){
         let response = await axios.get('http://127.0.0.1:8000/music/')
         console.log(response.data)
-        this.setState({
-            songs: response.data,
-            currentSongs: this.state.songs
-            
-        });
+        setSongs(response.data);
     }
 
-    runApp(){
-        if(this.state.songs.length === 0){
-            this.getAllSongs();
-        }
-        // this.filteredSearch();
-    }
 
-    // mapSongs(){
-    //    let temp entry.filter()
-    //     return entry.map(song =>
-    //         <Song
-    //             key={song.id}
-    //             song={song}
-    //             delete = {() => this.deleteSong(song.id)}
-    //         />
-    //     )
-    // }
-
-    mapSongs(songs){
-        let tempSongs = songs.filter(song => song.title.includes(this.state.filterTerm) || song.album.includes(this.state.filterTerm) || song.artist.includes(this.state.filterTerm) || song.genre.includes(this.state.filterTerm) || song.release_date.includes(this.state.filterTerm));
+    function mapSongs(songs){
+        let tempSongs = songs.filter(song => song.title.includes(filterTerm) || song.album.includes(filterTerm) || song.artist.includes(filterTerm) || song.genre.includes(filterTerm) || song.release_date.includes(filterTerm));
             return tempSongs.map(song =>
-                    <Song
-                        key={song.id}
-                        song={song}
-                        delete = {() => this.deleteSong(song.id)}
-                    />
-                )
+                <Song
+                    key={song.id}
+                    song={song}
+                    delete = {() => deleteSong(song.id)}
+                />
+            )
     }
 
-    async addNewSong(song){
+    async function addNewSong(song){
         let response = await axios.post('http://127.0.0.1:8000/music/', song) 
         console.log(response.data)
         alert('song has been added')
-        this.getAllSongs();
+        // this.getAllSongs();
     }
     
     async function deleteSong(songID) {
         let response = await axios.delete(`http://127.0.0.1:8000/music/${songID}/`)
         console.log(response.data)
         alert('song has been deleted')
-        this.getAllSongs();
     }
 
+    useEffect(() =>{
+        getAllSongs();
+    },[])
 
-        render () {
-            this.runApp();
-        return (
-            <div>
-                <div className="row row-spacer">
-                    <div className="col-md-12" style={{padding: 0}}>
-                        <div className='titlebar-nav'>
-                            <h1>React Music Library</h1>
-                        </div>
+    return (
+        <div>
+            <div className="row row-spacer">
+                <div className="col-md-12" style={{padding: 0}}>
+                    <div className='titlebar-nav'>
+                        <h1>React Music Library</h1>
                     </div>
                 </div>
-                <div>
-                <input
-                    style={styling}
-                    value={this.state.filterTerm}
-                    placeholder={'Search for a song'}
-                    onChange={this.setState(this.state.filterTerm)}
-                />
-                </div>
-                <div>
-                    <SongTable mapSongs={() => this.mapSongs(this.state.currentSongs)}/>
-                </div>
-                <div>
-                    <SongCreator addNewSong={this.addNewSong.bind(this)} />
-                </div>
             </div>
-        );
-    }
+            <div>
+            <input
+                style={styling}
+                value={filterTerm}
+                placeholder='Search for a song'
+                onChange={setFilterTerm(filterTerm)}
+            />
+            </div>
+            <div>
+                <SongTable mapSongs={() => mapSongs(songs)}/>
+            </div>
+            <div>
+                <SongCreator addNewSong={addNewSong.bind(this)} />
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
